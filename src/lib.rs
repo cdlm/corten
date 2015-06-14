10640fd2514fb9179a9d6cae04bb074ec6ca0a37
Damien Pollet <damien.pollet@gmail.com>
2015-06-14 21:56:21 +0200

    Default initialization for Interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 type Stack<T> = Vec<T>;
 
+#[derive(Default)]
 pub struct Interpreter<T> {
     data_stack: Stack<T>,
 }
 
 impl<T> Interpreter<T> {
-
-    fn new() -> Interpreter<T> {
-        Interpreter {
-            data_stack: Stack::new(),
-        }
-    }
 
     fn push(&mut self, value: T) {
         self.data_stack.push(value)
@@ -21,19 +16,20 @@ impl<T> Interpreter<T> {
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn pops_nothing() {
-        let mut interpreter = Interpreter::<i32>::new();
+        let mut interpreter = Interpreter::<i32>::default();
         assert_eq!(None, interpreter.pop())
     }
 
     #[test]
     fn pops_as_pushed() {
-        let mut interpreter = Interpreter::<i32>::new();
+        let mut interpreter = Interpreter::<i32>::default();
         interpreter.push(42);
         assert_eq!(Some(42), interpreter.pop());
         assert_eq!(None, interpreter.pop())
@@ -41,7 +37,7 @@ mod tests {
 
     #[test]
     fn pops_in_reverse_order() {
-        let mut interpreter = Interpreter::<i32>::new();
+        let mut interpreter = Interpreter::<i32>::default();
         interpreter.push(42);
         interpreter.push(51);
         assert_eq!(Some(51), interpreter.pop());
