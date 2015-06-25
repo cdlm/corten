202ba32047733504d59b2a46b89a1649028178a3
Geoffroy Couprie <geo.couprie@gmail.com>
2015-06-25 23:28:23 +0200

    does lifetime have a meaning?

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,12 +4,12 @@ use std::collections::HashMap;
 type Stack<T> = Vec<T>;
 
 #[derive(Default)]
-pub struct Interpreter<'a, T> {
+pub struct Interpreter<'a, 'b, T: 'b> {
     data_stack: Stack<T>,
-    vocabulary: HashMap<&'a str, Word<T>>,
+    vocabulary: HashMap<&'a str, &'b Word<T>>,
 }
 
-impl<'a, T> Interpreter<'a, T> where T: FromStr {
+impl<'a, 'b, T: 'b> Interpreter<'a, 'b, T> where T: FromStr {
 
     fn push(&mut self, value: T) {
         self.data_stack.push(value)
@@ -22,14 +22,18 @@ impl<'a, T> Interpreter<'a, T> where T: FromStr {
     fn eval_token(&mut self, token: &str) {
         if let Ok(literal) = token.parse::<T>() {
             self.push(literal);
+        } else {
+            if let Some(&word) = self.vocabulary.get(token) {
+                word.eval_within(self);
+            }
         }
     }
 
-    fn define_word(&mut self, name: &str, word: &Word<T>) {
-
+    fn define_word(&mut self, name: &'a str, word: &'b Word<T>) {
+        self.vocabulary.insert(name, word);
     }
 
-    fn lookup(&self, token: &str) -> Option<&Word<T>> {
+    fn lookup(&self, token: &str) -> Option<&&Word<T>> {
         self.vocabulary.get(token)
     }
 }
@@ -39,8 +43,8 @@ struct Word<T> {
 }
 
 impl<T> Word<T> {
-    fn eval_within(&self, i: &mut Interpreter<T>) {
-        (self.entry)(i)
+    fn eval_within(&self, mut i: &mut Interpreter<T>) {
+      (self.entry)(&mut i)
     }
 }
 
@@ -52,7 +56,7 @@ mod tests {
 
     fn fixture_plus() -> Word<i32> {
         Word {
-            entry: Box::new(move |i| {
+            entry: Box::new(|ref mut i| {
                 if let (Some(b), Some(a)) = (i.pop(), i.pop()) {
                     i.push(a + b)
                 } else { panic!("Stack underflow") }
@@ -62,8 +66,8 @@ mod tests {
 
     #[test]
     fn register_word() {
-        let mut interpreter = Interpreter::<i32>::default();
         let plus = fixture_plus();
+        let mut interpreter = Interpreter::<i32>::default();
         interpreter.define_word("+", &plus);
         interpreter.push(51);
         interpreter.push(42);
